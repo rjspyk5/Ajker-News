108e79ac2ab1b5ce9fef057fcb3fa505535b0e4f
rjspyk5 <rjspyk5@gmail.com>
2024-04-04 15:14:13 +0600

    header section complete

--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import logo from "../../assets/img/logo.png";
+import moment from "moment";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="text-center space-y-3">
+      <img className="mx-auto" src={logo} alt="" />
+      <p className="text-[#706F6F] font-normal">
+        Journalism Without Fear or Favour
+      </p>
+      <p>{moment().format("dddd, MMMM D ,YYYY")}</p>
+    </div>
+  );
 };
 
 export default Header;
