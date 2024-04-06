7b68c24906288fe8bca301835af378a933305d44
rjspyk5 <rjspyk5@gmail.com>
2024-04-06 23:44:19 +0600

    Error page added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import { Root } from "../layout/Root";
 import { Home } from "../pages/Home/Home";
 import { About } from "../pages/About/About";
 import { Career } from "../pages/Career/Career";
+import { ErrorPage } from "../pages/ErrorPage/ErrorPage";
 export const routes = createBrowserRouter([
   {
     path: "/",
@@ -19,6 +20,10 @@ export const routes = createBrowserRouter([
       {
         path: "/career",
         element: <Career />,
+      },
+      {
+        path: "*",
+        element: <ErrorPage />,
       },
     ],
   },
