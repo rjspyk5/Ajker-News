7ce431915b874850801e5d6c68ffbc816ff09c64
rjspyk5 <rjspyk5@gmail.com>
2024-04-10 02:16:43 +0600

    sign in and sign up function created and also authstatechanged fucntion created

--- src/Providers/Authprovider.jsx
+++ src/Providers/Authprovider.jsx
@@ -1,11 +1,33 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { PropTypes } from "prop-types";
+import auth from "./../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 export const authContext = createContext();
 
 export const Authprovider = ({ children }) => {
   const [user, setuser] = useState(null);
-  const contextValues = { user };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) =>
+      setuser(currentUser)
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const contextValues = { user, createUser, logIn, setuser };
   return (
     <div>
       <authContext.Provider value={contextValues}>
