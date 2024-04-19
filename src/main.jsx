8b82e587b6135a68cc3a140367cdaa3478a051d3
rjspyk5 <rjspyk5@gmail.com>
2024-04-19 22:18:12 +0600

    install animate css and import it to main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/Routes.jsx";
 import { Authprovider } from "./Providers/Authprovider.jsx";
-
+import "animate.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Authprovider>
